cf3a62338ce2e8257cf3be15aafb59ffb4f2497d
Alyona <besssta888@mail.ru>
2024-02-12 17:00:55 +0300

    feat: add main layout

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -2,10 +2,28 @@ import React from 'react';
 import cn from 'classnames';
 
 import styles from './mainPage.module.scss';
+import { Layout } from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 export const MainPage: React.FC = () => {
-
     return (
-        <div className={styles[cn('wrapper')]}>wrapper</div>
+        <div className={styles[cn('wrapper')]}>
+            <Layout
+                style={{
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100vh',
+                    backgroundColor: 'inherit',
+                }}
+            >
+                <Sider style={{ backgroundColor: 'inherit' }}>Sider</Sider>
+                <Layout style={{ backgroundColor: 'inherit' }}>
+                    <Header style={{ backgroundColor: 'inherit' }}>Header</Header>
+                    <Content style={{ backgroundColor: 'inherit' }}>Content</Content>
+                    <Footer style={{ backgroundColor: 'inherit' }}>Footer</Footer>
+                </Layout>
+            </Layout>
+        </div>
     );
 };
