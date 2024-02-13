0396313be31411d259d238a4101610f4370f4805
Alyona <besssta888@mail.ru>
2024-02-13 13:04:50 +0300

    feat: add exit button

--- src/pages/main-page/components/siderComponent/siderComponent.tsx
+++ src/pages/main-page/components/siderComponent/siderComponent.tsx
@@ -6,7 +6,7 @@ import {
     MenuUnfoldOutlined,
     TrophyFilled,
 } from '@ant-design/icons';
-import { Layout, Menu } from 'antd';
+import { Button, Layout, Menu } from 'antd';
 import cn from 'classnames';
 import React, { useState } from 'react';
 
@@ -39,6 +39,8 @@ const MENU_ITEMS = [
     },
 ];
 
+const EXIT_BUTTON = 'Выход';
+
 const { Sider } = Layout;
 
 export const SiderComponent: React.FC = () => {
@@ -48,50 +50,72 @@ export const SiderComponent: React.FC = () => {
         setCollapsed(!collapsed);
     };
     return (
-        <Sider
-            className={styles[cn('sider')]}
-            collapsed={collapsed}
-            collapsible
-            style={{ backgroundColor: 'var(--backgroud-color-element)' }}
-            trigger={null}
-        >
-            <a href='#' onClick={(e) => e.preventDefault()}>
-                <img
-                    alt='logo'
-                    src={
-                        collapsed
-                            ? '/src/assets/images/logo_short.svg'
-                            : '/src/assets/images/logo_long.svg'
-                    }
+        <>
+            <Sider
+                className={styles[cn('sider')]}
+                collapsed={collapsed}
+                collapsible
+                style={{ backgroundColor: 'var(--backgroud-color-element)' }}
+                trigger={null}
+            >
+                <a href='#' onClick={(e) => e.preventDefault()}>
+                    <img
+                        alt='logo'
+                        src={
+                            collapsed
+                                ? '/src/assets/images/logo_short.svg'
+                                : '/src/assets/images/logo_long.svg'
+                        }
+                    />
+                </a>
+                <Menu
+                    style={{ color: 'var(--color-title)' }}
+                    items={MENU_ITEMS.map((item) => {
+                        return {
+                            key: item.key,
+                            icon: (
+                                <item.icon
+                                    style={{ color: 'var(--color-elements)' }}
+                                    twoToneColor={
+                                        item.isTwoTonesIcon ? 'var(--color-elements)' : undefined
+                                    }
+                                />
+                            ),
+                            label: item.label,
+                        };
+                    })}
                 />
-            </a>
-            <Menu style={{color: 'var(--color-title)'}}
-                items={MENU_ITEMS.map((item) => {
-                    return {
-                        key: item.key,
-                        icon: (
-                            <item.icon
-                                style={{ color: 'var(--color-elements)' }}
-                                twoToneColor={
-                                    item.isTwoTonesIcon ? 'var(--color-elements)' : undefined
-                                }
-                            />
-                        ),
-                        label: item.label,
-                    };
-                })}
-            />
-            {collapsed ? (
-                <MenuUnfoldOutlined
-                    className={styles[cn('sider_button')]}
-                    onClick={handleSiderButton}
-                />
-            ) : (
-                <MenuFoldOutlined
-                    className={styles[cn('sider_button')]}
-                    onClick={handleSiderButton}
-                />
-            )}
-        </Sider>
+                {collapsed ? (
+                    <MenuUnfoldOutlined
+                        className={styles[cn('sider_button')]}
+                        onClick={handleSiderButton}
+                    />
+                ) : (
+                    <MenuFoldOutlined
+                        className={styles[cn('sider_button')]}
+                        onClick={handleSiderButton}
+                    />
+                )}
+                <Button
+                    style={{
+                        position: 'absolute',
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        width: '100px',
+                        color: 'var(--color-title)'
+                    }}
+                    className={styles[cn('exit_button')]}
+                    type='text'
+                >
+                    <img
+                        alt='exit_icon'
+                        className={styles[cn('exit_icon')]}
+                        src='/src/assets/images/exit_icon.svg'
+                    />
+                    {!collapsed && EXIT_BUTTON}
+                </Button>
+            </Sider>
+        </>
     );
 };
