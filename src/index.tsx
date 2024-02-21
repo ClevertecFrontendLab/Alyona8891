9257ede9293abedda66a06acada2d03ea22cd101
Alyona8891 <besssta888@mail.ru>
2024-02-21 15:53:58 +0300

    feat: add HistoryRouter to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HistoryRouter } from 'redux-first-history/rr6';
+import { Route, Routes } from 'react-router-dom';
 
-import { store } from '@redux/configure-store';
+import { store, history } from '@redux/configure-store';
 import { MainPage } from './pages';
 
 import 'antd/dist/antd.css';
@@ -16,11 +17,11 @@ const root = createRoot(domNode);
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <HashRouter>
+            <HistoryRouter history={history}>
                 <Routes>
                     <Route path='/' element={<MainPage />} />
                 </Routes>
-            </HashRouter>
+            </HistoryRouter>
         </Provider>
     </React.StrictMode>,
 );
--- src/redux/configure-store.ts
+++ src/redux/configure-store.ts
@@ -1,4 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { createBrowserHistory } from 'history'
+
+export const history = createBrowserHistory();
 
 export const store = configureStore({
     reducer: {},
