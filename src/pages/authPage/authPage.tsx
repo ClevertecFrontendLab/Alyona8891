e7025377ee438bbef6386e1c63a88c32bf8b1d64
Alyona8891 <besssta888@mail.ru>
2024-02-21 17:53:11 +0300

    feat: add main layout of authPage

--- src/pages/authPage/authPage.tsx
+++ src/pages/authPage/authPage.tsx
@@ -6,7 +6,15 @@ import styles from './authPage.module.scss';
 export const AuthPage: React.FC = () => {
     return (
         <div className={styles[cn('wrapper')]}>
-            AUTH
+            <section className={styles[cn('container')]}>
+                <div className={styles[cn('logo_block')]}>
+                    <img
+                        className={styles[cn('logo')]}
+                        alt='logo'
+                        src='/src/assets/images/logo_long.svg'
+                    />
+                </div>
+            </section>
         </div>
     );
 };
