f0f01ce3587e3814f5111249da7b0c19a6b34153
Alyona8891 <besssta888@mail.ru>
2024-02-23 20:25:13 +0300

    feat: add form

--- src/pages/authPage/components/signInContent/signInContent.tsx
+++ src/pages/authPage/components/signInContent/signInContent.tsx
@@ -2,8 +2,47 @@ import React from 'react';
 import cn from 'classnames';
 
 import styles from './signInContent.module.scss';
+import { Button, Checkbox, Form, Input } from 'antd';
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 
 export const SignInContent: React.FC = () => {
+    return (
+        <Form
+            className={styles[cn('form')]}
+            name='basic'
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            onFinish={() => {}}
+            onFinishFailed={() => {}}
+            autoComplete='off'
+        >
+            <Form.Item
+                name='username'
+                rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+                <Input className={styles[cn('input')]} addonBefore='e-mail:' />
+            </Form.Item>
 
-    return <div>Content</div>;
+            <Form.Item
+                name='password'
+                rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+                <Input.Password
+                    placeholder='Пароль'
+                    iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                />
+            </Form.Item>
+
+            <Form.Item name='remember' valuePropName='checked' wrapperCol={{ offset: 8, span: 16 }}>
+                <Checkbox>Запомнить меня</Checkbox>
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type='primary' htmlType='submit'>
+                    Войти
+                </Button>
+            </Form.Item>
+        </Form>
+    );
 };
