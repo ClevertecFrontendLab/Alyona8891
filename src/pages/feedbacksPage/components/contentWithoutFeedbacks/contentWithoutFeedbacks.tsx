f0cb2f8a0bc0733cc1c56edc4e8435161a84a5f2
Alyona8891 <besssta888@mail.ru>
2024-03-02 20:14:09 +0300

    feat: add inner content to contentWithoutFeedbacks

--- src/pages/feedbacksPage/components/contentWithoutFeedbacks/contentWithoutFeedbacks.tsx
+++ src/pages/feedbacksPage/components/contentWithoutFeedbacks/contentWithoutFeedbacks.tsx
@@ -1,3 +1,40 @@
+import cn from 'classnames';
+import styles from './contentWithoutFeedbacks.module.scss';
+import { Content } from 'antd/lib/layout/layout';
+import { Button, Card, Space, Typography } from 'antd';
+const { Text, Title } = Typography;
+
+const CONTENT_WITHOUT_FEEDBACKS = {
+    title: 'Оставьте свой отзыв первым',
+    text: [
+        {
+            key: 1,
+            part: 'Вы можете быть первым, кто оставит отзыв об этом фитнесс приложении.',
+        },
+        { key: 2, part: 'Поделитесь своим мнением и опытом с другими пользователями,' },
+        { key: 3, part: 'и помогите им сделать правильный выбор.' },
+    ],
+    button: 'Написать отзыв',
+};
+
 export const ContentWithoutFeedbacks = () => {
-    return <div>Without</div>;
+    return (
+        <Content className={styles[cn('wrapper')]}>
+            <Card className={styles[cn('card')]} headStyle={{ textAlign: 'center' }}>
+                <Title className={styles[cn('title')]} level={3}>
+                    {CONTENT_WITHOUT_FEEDBACKS.title}
+                </Title>
+                <Space direction='vertical' size={0}>
+                    {CONTENT_WITHOUT_FEEDBACKS.text.map((text) => (
+                        <Text className={styles[cn('text')]} key={text.key} type='secondary'>
+                            {text.part}
+                        </Text>
+                    ))}
+                </Space>
+            </Card>
+            <Button className={styles[cn('button')]} type='primary' size='large'>
+                {CONTENT_WITHOUT_FEEDBACKS.button}
+            </Button>
+        </Content>
+    );
 };
