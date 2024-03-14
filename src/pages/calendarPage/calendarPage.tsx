8bc6249232034af37b44d311fc4c8b0fa69ef4ef
Alyona8891 <besssta888@mail.ru>
2024-03-14 14:53:39 +0300

    feat: add footer & content to calendarPage

--- src/pages/calendarPage/calendarPage.tsx
+++ src/pages/calendarPage/calendarPage.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 
 import { Calendar } from 'antd';
+import useBreakpoint from 'antd/es/grid/hooks/useBreakpoint';
 
 import { RouterPath } from '@constants/constants';
 import { MainLayout } from '@layouts/mainLayout';
 import { HeaderComponent } from '@pages/ui/headerComponent';
+import { Content, Footer } from 'antd/lib/layout/layout';
 
 const routes = [
     {
@@ -20,10 +22,23 @@ const routes = [
 ];
 
 export const CalendarPage: FC = () => {
+    const breakpoint = useBreakpoint();
+
     return (
         <MainLayout>
             <HeaderComponent routes={routes} />
-            <Calendar />
+            <Content  style={{
+                minHeight: 'initial',
+                height: '100%',
+                backgroundColor: 'var(--background-color-header)',
+                padding: breakpoint.xs ? '1.5rem 1rem 1rem' : '1.5rem 0 0 1.5rem',
+                flex: '1 1 auto',
+            }}>
+                <Calendar />
+            </Content>
+            <Footer  style={{
+                backgroundColor: 'inherit',
+            }}/>
         </MainLayout>
     );
 };
