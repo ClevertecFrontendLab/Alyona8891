c606cc390d7f6248e2e961a6a549e06eb38b2c6f
Alyona8891 <besssta888@mail.ru>
2024-03-14 16:37:55 +0300

    feat: change locales

--- src/pages/calendarPage/calendarPage.tsx
+++ src/pages/calendarPage/calendarPage.tsx
@@ -2,11 +2,44 @@ import { FC } from 'react';
 
 import { Calendar } from 'antd';
 import useBreakpoint from 'antd/es/grid/hooks/useBreakpoint';
+import locale from 'antd/es/date-picker/locale/ru_RU';
+import { Content, Footer } from 'antd/lib/layout/layout';
+
+import moment from 'moment';
+moment.updateLocale('ru', {
+    week: {
+        dow: 1,
+        doy: 0,
+    },
+});
 
 import { RouterPath } from '@constants/constants';
 import { MainLayout } from '@layouts/mainLayout';
 import { HeaderComponent } from '@pages/ui/headerComponent';
-import { Content, Footer } from 'antd/lib/layout/layout';
+
+const calendarLocale = {
+    lang: {
+        ...locale.lang,
+        shortWeekDays: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+        shortMonths: [
+            'Янв',
+            'Фев',
+            'Мар',
+            'Апр',
+            'Май',
+            'Июн',
+            'Июл',
+            'Авг',
+            'Сен',
+            'Окт',
+            'Ноя',
+            'Дек',
+        ],
+    },
+    timePickerLocale: {
+        ...locale.timePickerLocale,
+    },
+};
 
 const routes = [
     {
@@ -27,18 +60,22 @@ export const CalendarPage: FC = () => {
     return (
         <MainLayout>
             <HeaderComponent routes={routes} />
-            <Content  style={{
-                minHeight: 'initial',
-                height: '100%',
-                backgroundColor: 'var(--background-color-header)',
-                padding: breakpoint.xs ? '1.5rem 1rem 1rem' : '1.5rem 0 0 1.5rem',
-                flex: '1 1 auto',
-            }}>
-                <Calendar />
+            <Content
+                style={{
+                    minHeight: 'initial',
+                    height: '100%',
+                    backgroundColor: 'var(--background-color-header)',
+                    padding: breakpoint.xs ? '1.5rem 1rem 1rem' : '1.5rem 0 0 1.5rem',
+                    flex: '1 1 auto',
+                }}
+            >
+                <Calendar locale={calendarLocale} defaultValue={moment()} />
             </Content>
-            <Footer  style={{
-                backgroundColor: 'inherit',
-            }}/>
+            <Footer
+                style={{
+                    backgroundColor: 'inherit',
+                }}
+            />
         </MainLayout>
     );
 };
